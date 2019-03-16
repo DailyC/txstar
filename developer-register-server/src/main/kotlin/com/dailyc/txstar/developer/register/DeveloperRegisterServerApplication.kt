07d4b0e0d9269a96999ff42d359b9434b26662a5
wangweiwei12 <wangweiwei12@jd.com>
2019-03-16 20:52:14 +0800

    增加开发者平台注册中心

---
+++ developer-register-server/src/main/kotlin/com/dailyc/txstar/developer/register/DeveloperRegisterServerApplication.kt
@@ -0,0 +1,11 @@
+package com.dailyc.txstar.developer.register
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeveloperRegisterServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<DeveloperRegisterServerApplication>(*args)
+}
