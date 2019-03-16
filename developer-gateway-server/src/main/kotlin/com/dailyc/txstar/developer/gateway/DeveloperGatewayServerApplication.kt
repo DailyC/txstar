6a540fe8da362e0c81074e3729654e8eb2f467f3
wangweiwei12 <wangweiwei12@jd.com>
2019-03-17 00:14:49 +0800

    增加开发者平台注册中心

---
+++ developer-gateway-server/src/main/kotlin/com/dailyc/txstar/developer/gateway/DeveloperGatewayServerApplication.kt
@@ -0,0 +1,11 @@
+package com.dailyc.txstar.developer.gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeveloperGatewayServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<DeveloperGatewayServerApplication>(*args)
+}
---
+++ developer-gateway-server/src/test/kotlin/com/dailyc/txstar/developer/gateway/DeveloperGatewayServerApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.dailyc.txstar.developer.gateway
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class DeveloperGatewayServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
