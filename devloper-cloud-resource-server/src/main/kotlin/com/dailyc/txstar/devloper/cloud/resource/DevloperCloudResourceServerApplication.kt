2d781b4850a1b0c41b26492f32f4f98dcda23d82
weiwei <weiwei02@vip.qq.com>
2019-03-17 23:28:36 +0800

    创建云资源管理工程

---
+++ devloper-cloud-resource-server/src/main/kotlin/com/dailyc/txstar/devloper/cloud/resource/DevloperCloudResourceServerApplication.kt
@@ -0,0 +1,11 @@
+package com.dailyc.txstar.devloper.cloud.resource
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevloperCloudResourceServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<DevloperCloudResourceServerApplication>(*args)
+}
---
+++ devloper-cloud-resource-server/src/test/kotlin/com/dailyc/txstar/devloper/cloud/resource/DevloperCloudResourceServerApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.dailyc.txstar.devloper.cloud.resource
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class DevloperCloudResourceServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
