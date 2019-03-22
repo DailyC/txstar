073e0b3fe1786cc0cc8f9f070d693b8b2201c170
wangweiwei12 <wangweiwei12@jd.com>
2019-03-22 16:09:28 +0800

    创建开发者平台共用api工程

---
+++ developer-cloud-common-api/src/main/kotlin/com/dailyc/txstar/developer/cloud/api/package-info.java
@@ -0,0 +1,7 @@
+/** 开发者平台，共用api组件
+ *
+ * @author Wang Weiwei / email : wangweiwei12@jd.com
+ * @version 17.8.0
+ * @since 2019-03-22
+ */
+package com.dailyc.txstar.developer.cloud.api;
---
+++ developer-cloud-common-api/src/test/kotlin/com/dailyc/txstar/developer/cloud/resource/server/DeveloperCloudResourceServerApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.dailyc.txstar.developer.cloud.resource.server
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class DeveloperCloudResourceServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
